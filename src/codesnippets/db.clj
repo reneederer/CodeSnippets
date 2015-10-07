ff48cd1be0ae3bd56a1b077347397f5ddb50c6f9
Rene Ederer <reneederer@arcor.de>
2015-10-07 09:42:33 +0200

    Removed compile details from the source code to the database

--- src/codesnippets/db.clj
+++ src/codesnippets/db.clj
@@ -1,38 +1,38 @@
 (ns codesnippets.db
-    (:require [clojure.java [jdbc :as sql]]))
+  (:require [clojure.java [jdbc :as sql]]))
 
 (def db  {:classname "com.mysql.jdbc.Driver"
-                  :subprotocol "mysql"
-                   :subname "//localhost/codesnippets"
-                  :user "root"
-                  :password "1234"})
+          :subprotocol "mysql"
+          :subname "//localhost/codesnippets"
+          :user "root"
+          :password "1234"})
 
- (defn get-prog-langs
-       "Queries the database and gets the programming languages"
-       []
-       (sql/query db  ["select name from programming_language"]))
+(defn get-prog-langs
+  "Queries the database and gets the programming languages"
+  []
+  (sql/query db  ["select name from programming_language"]))
 
 (defn get-snippet-names
   "Gets the snippet titles for the specified programming language"
   [lang]
   (sql/query db [(str "select title from codesnippet
-                 join programming_language_version
-                   on codesnippet.programming_language_version_id = programming_language_version.id
-                 join programming_language
-                   on programming_language_version.programming_language_id = programming_language.id
-                 where programming_language.name = '" lang "'")]))
- 
-(defn get-source
-     "Queries the database and returns the sourcecode"
-     [lang title]
-  (sql/query db  [(str "select sourcecode from codesnippet
-                   join programming_language_version
-                   on codesnippet.programming_language_version_id = programming_language_version.id
-                   join programming_language
-                   on programming_language_version.programming_language_id = programming_language.id
-                   where programming_language.name = '" lang "'
-                                                             and codesnippet.title = '" title "'")]))
- 
+                      join programming_language_version
+                      on codesnippet.programming_language_version_id = programming_language_version.id
+                      join programming_language
+                      on programming_language_version.programming_language_id = programming_language.id
+                      where programming_language.name = '" lang "'")]))
 
-              
+(defn get-source
+  "Queries the database and returns the sourcecode"
+  [lang title]
+  (sql/query db  [(str "select sourcecode from codesnippet
+                       join programming_language_version
+                       on codesnippet.programming_language_version_id = programming_language_version.id
+                       join programming_language
+                       on programming_language_version.programming_language_id = programming_language.id
+                       where programming_language.name = '" lang "'
+                       and codesnippet.title = '" title "'")]))
+
+
+
 
--- src/codesnippets/execute.clj
+++ src/codesnippets/execute.clj
@@ -1,13 +1,40 @@
 (ns codesnippets.execute
+  (:require [clojure.java.io :as io])
            (:require [me.raynes.conch.low-level :as shell]))
 
-(def srcFolder "~/dev/clojure/codesnippets/src/codesnippets/")
+(def srcFolder "/home/rene/dev/clojure/codesnippets/src/codesnippets/")
+
 
 (defn compileAndRun
   [source]
-  (spit  (srcFolder "test.cob")  source)
-  (let [errors (shell/stream-to-string
-                 (shell/proc "cobc" "-x"  (str srcFolder "test.cob") "-o"  (str srcFolder "test")) :err)
-        output (shell/stream-to-string
-                 (shell/proc  (str srcFolder "test")) :out)]
-    {:errors errors :output output}))
+  (print source)
+  (spit  (str srcFolder "test.cob")  source)
+  (let  [errors  (shell/stream-to-string
+                   (shell/proc "cobc" "-x"  (str srcFolder "test.cob") "-o"  (str srcFolder "test")) :err)]
+    (let [output  (if (io/as-file (str srcFolder "test"))
+                   (shell/stream-to-string (shell/proc  (str srcFolder "test")) :out)
+                   nil)]
+
+    {:errors errors :output output} ))
+  
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
